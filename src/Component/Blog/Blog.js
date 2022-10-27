d83bedb2933b6898600cd5607d5e49805578420c
Minhaz-Khan <kminhaz111@gmail.com>
2022-10-27 11:43:54 +0600

    blog done

--- src/Component/Blog/Blog.js
+++ src/Component/Blog/Blog.js
@@ -5,8 +5,8 @@ const Blog = () => {
         <div>
             <div className="max-w-md p-6 overflow-hidden rounded-lg shadow dark:bg-gray-900 dark:text-gray-100">
                 <article>
-                    <h2 className="text-xl font-bold">Sed diam massa, semper a condimentum</h2>
-                    <p className="mt-4 dark:text-gray-400">Morbi porttitor mi in diam scelerisque commodo. Proin sed laoreet libero. Fusce faucibus porttitor lacus, at blandit mauris blandit eget. Donec facilisis lorem et risus commodo, quis auctor nulla varius. Pellentesque facilisis feugiat turpis, id molestie augue semper quis. Nunc ornare eget est sit amet elementum.</p>
+                    <h2 className="text-xl font-bold">What is CORS ?</h2>
+                    <p className="mt-4 dark:text-gray-400">Cross-origin resource sharing (CORS) is a browser mechanism which enables controlled access to resources located outside of a given domain. It extends and adds flexibility to the same-origin policy (SOP). However, it also provides potential for cross-domain attacks, if a website's CORS policy is poorly configured and implemented. CORS is not a protection against cross-origin attacks such as cross-site request forgery (CSRF).</p>
                     <div className="flex items-center mt-8 space-x-4">
                         <img src="https://source.unsplash.com/100x100/?portrait" alt="" className="w-10 h-10 rounded-full dark:bg-gray-500" />
                         <div>
